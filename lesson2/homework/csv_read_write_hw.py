06057ff568fc9fdce479399dd1032909533b10a8
Nastya Krasun <nastyakrasun@gmail.com>
2021-11-26 10:24:21 +0300

    teacher's solution of .csv task

--- lesson2/homework/csv_read_write_hw.py
+++ lesson2/homework/csv_read_write_hw.py
@@ -25,7 +25,15 @@ info_1.txt, info_2.txt, info_3.txt
 from chardet import detect  # для определения кодировки
 import re  # для работы с рег выражениями
 import csv  # для работы с csv
+import chardet
 
+
+# from contextlib import ExitStack  # для открытия кучи файлов (не сработало с кодировкой)
+# from itertools import zip_longest
+
+# flist = ['info_1.txt', 'info_2.txt', 'info_3.txt'] with ExitStack() as stack: files = [stack.enter_context(open(
+# fname)) for fname in flist] for lines in zip_longest(*files): print(lines)
+# UnicodeDecodeError: 'utf-8' codec can't decode byte 0xc8 in position 0: invalid continuation byte
 
 # from contextlib import ExitStack  # для открытия кучи файлов (не сработало с кодировкой)
 # from itertools import zip_longest
@@ -98,3 +106,73 @@ with open('hw_data_read.csv', encoding='utf-8') as f_n:  # открываем ф
     for row in F_N_READER:
         print(row)  # открываем как список
         # последняя пустая строка не удаляется
+
+
+# # решение препода
+# def get_data():
+#     # get data from .txt-file
+#     os_prod_list = []
+#     os_name_list = []
+#     os_code_list = []
+#     os_type_list = []
+#     main_data = []
+#
+#     for i in range(1, 4):
+#         with open(f'info_{i}.txt', 'rb') as file_obj:
+#             data_bytes = file_obj.read()
+#             result = chardet.detect(data_bytes)
+#             data = data_bytes.decode(result['encoding'])
+#
+#         # get a list of OS manufactures
+#         os_prod_reg = re.compile(r'Изготовитель системы:\s*\S*')
+#         os_prod_list.append(os_prod_reg.findall(data)[0].split()[2])
+#
+#         # get a list of OS names
+#         os_name_reg = re.compile(r'Windows\s*\S*')
+#         os_name_list.append(os_name_reg.findall(data)[0])
+#
+#         # get a list of products code
+#         os_code_reg = re.compile(r'Код продукта:\s*\S*')
+#         os_code_list.append(os_code_reg.findall(data)[0].split()[2])
+#
+#         # get a list of system type
+#         os_type_reg = re.compile(r'Тип системы:\s*\S*')
+#         os_type_list.append(os_type_reg.findall(data)[0].split()[2])
+#
+#     headers = ['Изготовитель системы', 'Название ОС', 'Код продукта', 'Тип системы']
+#     main_data.append(headers)
+#
+#     data_for_rows = [os_prod_list, os_name_list, os_code_list, os_type_list]
+#
+#     # make matrix (4 x 3) from matrix (3 x 4)
+#     for idx in range(len(data_for_rows[0])):
+#         line = [row[idx] for row in data_for_rows]
+#         main_data.append(line)
+#
+#     # variant 2
+#     # for idx in range((len(data_for_rows[0])):
+#     #     line = list(map(lambda row: row[idx], data_for_rows]
+#     #     main_data.append(line)
+#
+#     # variant 3
+#     # for idx in range((len(data_for_rows[0])):
+#     #     main_data.append([os_prod_list[idx], os_name_list[idx], os_code_list[idx], os_type_list[idx]])
+#
+#     # variant 4: zip-transformation
+#     # list(zip([1, 2, 3],                 [(1, 4),
+#     #          [4, 5, 6]))       ==>       (2, 5),
+#     #                                      (3, 6)]
+#
+#     return main_data
+#
+#
+# def write_to_csv(out_file):
+#     """Write the data to csv-file"""
+#     main_data = get_data()
+#     with open(out_file, 'w', encoding='utf-8') as file:
+#         writer = csv.writer(file)
+#         for row in main_data:
+#             writer.writerow(row)
+#
+#
+# write_to_csv('data_report.csv')
