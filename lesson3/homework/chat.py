1e51a9c2cdefe2124841a20636d5a51943339ee6
Nastya Krasun <nastyakrasun@gmail.com>
2021-12-02 13:04:08 +0300

    поправила пз к уроку3

--- lesson3/homework/chat.py
+++ lesson3/homework/chat.py
@@ -13,6 +13,17 @@ MAX_PACKAGE_LENGTH = 1024
 # Кодировка проекта
 ENCODING = 'utf-8'
 
+# Прококол JIM основные ключи:
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+
+# Прочие ключи, используемые в протоколе
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+
 
 def get_server_socket(addr, port):
     SERV_SOCK = socket.socket()
---
+++ lesson4/homework/variables.py
@@ -0,0 +1,54 @@
+"""Константы"""
+from datetime import datetime
+
+# Порт поумолчанию для сетевого ваимодействия
+DEFAULT_PORT = 8000
+# IP адрес по умолчанию для подключения клиента
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+# Максимальная очередь подключений
+MAX_CONNECTIONS = 5
+# Максимальная длинна сообщения в байтах
+MAX_PACKAGE_LENGTH = 1024
+# Кодировка проекта
+ENCODING = 'utf-8'
+
+# Прококол JIM основные ключи:
+ACCOUNT_NAME = 'account_name'
+ACTION = 'action'
+TIME = datetime.now().replace(microsecond=0).isoformat(sep=' ')
+USER = 'user'
+
+
+# Прочие ключи, используемые в протоколе
+ERROR = 'error'
+
+MESSAGE = {
+    "action": "msg",
+    "time": str(TIME),
+    "to": None,
+    "message": None
+}
+
+PRESENCE = {
+    "action": "presence",
+    "time": str(TIME),
+    "type": "status",
+    "user": {
+        "account_name": '',
+        "status": "осуществляется передача сообщений"
+    }
+}
+
+CLIENT_RESP = {
+    "response": 'Соединение установлено',
+    "time": str(TIME),
+    "alert": None,
+    "from": 'Server',
+    "contacts": None
+}
+
+SERV_RESP = (
+    ('200', 'OK'),
+    ('401', 'Не авторизован'),
+    ('404', 'Not found')
+)
